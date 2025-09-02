4c0c51a38950c3753440cd946fc406a384fd8c4c
AnonimProgrammer <zephyr11ff@gmail.com>
2025-09-02 23:24:08 +0400

    Updated with new fields represented available balance and request amount.

--- src/main/java/com/docker/mswallet/exception/InsufficientBalanceException.java
+++ src/main/java/com/docker/mswallet/exception/InsufficientBalanceException.java
@@ -1,6 +1,24 @@
 package com.docker.mswallet.exception;
 
+import java.math.BigDecimal;
+
 public class InsufficientBalanceException extends WalletException {
+
+    private BigDecimal availableBalance;
+    private BigDecimal requestedAmount;
+
+    public InsufficientBalanceException(String message, BigDecimal availableBalance, BigDecimal requestedAmount) {
+        super(message);
+        this.availableBalance = availableBalance;
+        this.requestedAmount = requestedAmount;
+    }
+
+    public BigDecimal getAvailableBalance() {
+        return availableBalance;
+    }
+    public BigDecimal getRequestedAmount() {
+        return requestedAmount;
+    }
 
     public InsufficientBalanceException(String message) {
         super(message);
